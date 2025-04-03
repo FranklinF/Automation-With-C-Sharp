c20d0cf1fc9289b64290f9d688a16b06ded57ec0
Franklin <franklin@Franklins-MacBook-Air.local>
2025-04-03 09:37:55 +0530

    automate folderfilecreation

---
+++ FolderAutomation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    public static void Main()
+    {
+        int i = 1;
+        for (i = 1; i <= 100; i++)
+        {
+            string folderpath;
+
+            if (i < 9)
+                folderpath = @"/Users/franklin/Desktop/100days-of-Phython/Demo0" + i;
+            else
+                folderpath = @"/Users/franklin/Desktop/100days-of-Phython/Demo" + i;
+
+            string file = "Readme.text";
+
+            Directory.CreateDirectory(folderpath);
+            string finalpath = Path.Combine(folderpath, file);
+
+            string data = "Demo File" + i;
+            File.WriteAllText(finalpath, data);
+        }
+    }
+}
